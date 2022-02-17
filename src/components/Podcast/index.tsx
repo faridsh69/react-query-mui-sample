24250609b8615e37247a84016b31c95b27fed936
Mostafa Ghafouri <mstghafouri@gmail.com>
2022-02-17 20:57:54 +0330

    Add panel & tabs to the podcast container

--- src/components/Podcast/index.tsx
+++ src/components/Podcast/index.tsx
@@ -9,91 +9,101 @@ import Badge from '@mui/material/Badge';
 import Chip from '@mui/material/Chip';
 
 import { SVG } from '@assets/icons/svgIcons';
+import Tabs from './Tabs';
+import PodcastPanel from './Panel';
 import { useStyles } from './styles';
 
 const Podcast: FC = () => {
   const { classes } = useStyles();
 
   return (
-    <div className={classes.banner}>
-      <div className={classes.filter}>&nbsp;</div>
+    <>
+      <div className={classes.banner}>
+        <div className={classes.filter}>&nbsp;</div>
 
-      <figure className={classes.figure}>
-        <Image priority src="/images/poster.png" layout="fill" alt="happy-place-podcast" />
-      </figure>
+        <figure className={classes.figure}>
+          <Image priority src="/images/poster.png" layout="fill" alt="happy-place-podcast" />
+        </figure>
 
-      <div className={classes.infoBox}>
-        <Stack direction="row" spacing={1} alignItems="center">
-          <Typography variant="h1" mr="0.5rem">
-            The Happy Place
+        <div className={classes.infoBox}>
+          <Stack direction="row" spacing={1} alignItems="center">
+            <Typography variant="h1" mr="0.5rem">
+              The Happy Place
+            </Typography>
+            <Chip color="primary" label="Claimed" icon={<SVG id="tick" />} />
+
+            <SVG id="sats" />
+          </Stack>
+
+          <Typography mb={1} variant="caption">
+            Health & Fitness
           </Typography>
-          <Chip color="primary" label="Claimed" icon={<SVG id="tick" />} />
+          <Typography>
+            Fearne Cotton talks to incredible people about life, love, loss, and everything
+            in-between as she reveals what happiness means to them.
+          </Typography>
 
-          <SVG id="sats" />
-        </Stack>
+          <Button aria-label="underline" color="secondary">
+            Read more
+          </Button>
 
-        <Typography mb={1} variant="caption">
-          Health & Fitness
-        </Typography>
-        <Typography>
-          Fearne Cotton talks to incredible people about life, love, loss, and everything in-between
-          as she reveals what happiness means to them.
-        </Typography>
+          <Button variant="contained" color="primary" className={classes.tBtn}>
+            PLAY TRAILER
+          </Button>
 
-        <Button sx={{ paddingLeft: 0 }} color="secondary">
-          <u>Read more</u>
-        </Button>
+          <Stack direction="row" spacing={6} mt="3rem" mb="2.7rem">
+            <Stack direction="row" spacing={1}>
+              <Avatar alt="Fearne Cotton" src="/images/host.png" />
 
-        <Button variant="contained" color="primary" className={classes.tBtn}>
-          PLAY TRAILER
-        </Button>
+              <Typography variant="subtitle1">
+                Host
+                <br />
+                <strong>Fearne Cotton</strong>
+              </Typography>
+            </Stack>
 
-        <Stack direction="row" spacing={6} mt="3rem" mb="2.7rem">
-          <Stack direction="row" spacing={1}>
-            <Avatar alt="Fearne Cotton" src="/images/host.png" />
+            <Stack direction="row" spacing={1}>
+              <Badge
+                overlap="circular"
+                anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+                badgeContent={<SVG id="crown" />}
+              >
+                <Avatar alt="John Smith" src="/images/king.png" />
+              </Badge>
 
-            <Typography variant="subtitle1">
-              Host
-              <br />
-              <strong>Fearne Cotton</strong>
-            </Typography>
+              <Typography variant="subtitle1">
+                Show King
+                <br />
+                <strong>John Smith</strong>
+              </Typography>
+            </Stack>
           </Stack>
 
+          <Typography variant="subtitle1">
+            2 Seasons • 88 Episodes • Weekly • V+ New Episodes First • 1,362 Followers
+          </Typography>
+        </div>
+
+        <Stack direction="row" spacing={1} className={`padding ${classes.cta}`}>
+          <Tabs />
+
           <Stack direction="row" spacing={1}>
-            <Badge
-              overlap="circular"
-              anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-              badgeContent={<SVG id="crown" />}
-            >
-              <Avatar alt="John Smith" src="/images/king.png" />
-            </Badge>
-
-            <Typography variant="subtitle1">
-              Show King
-              <br />
-              <strong>John Smith</strong>
-            </Typography>
+            <Button variant="contained">Follow</Button>
+            <IconButton>
+              <SVG id="like" />
+            </IconButton>
+            <IconButton>
+              <SVG id="share" />
+            </IconButton>
+            <IconButton>
+              <SVG id="more" />
+            </IconButton>
           </Stack>
-        </Stack>
-
-        <Typography variant="subtitle1">
-          2 Seasons • 88 Episodes • Weekly • V+ New Episodes First • 1,362 Followers
-        </Typography>
-
-        <Stack direction="row" spacing={1} className={classes.cta}>
-          <Button variant="contained">Follow</Button>
-          <IconButton>
-            <SVG id="like" />
-          </IconButton>
-          <IconButton>
-            <SVG id="share" />
-          </IconButton>
-          <IconButton>
-            <SVG id="more" />
-          </IconButton>
         </Stack>
       </div>
-    </div>
+
+      <PodcastPanel />
+    </>
   );
 };
 
