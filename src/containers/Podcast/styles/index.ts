e10223038513c0f599ee5af7bf4e306b439154da
Mostafa Ghafouri <mstghafouri@gmail.com>
2022-02-18 20:18:13 +0330

    Update podcast component styles

--- src/containers/Podcast/styles/index.ts
+++ src/containers/Podcast/styles/index.ts
@@ -1,28 +1,7 @@
 import { makeStyles } from 'tss-react/mui';
 
 export const useStyles = makeStyles()(() => ({
-  banner: {
-    height: '69rem',
-    backgroundImage: 'url(/images/poster.png)',
-    backgroundRepeat: 'no-repeat',
-    position: 'relative',
-    backgroundSize: '100%',
-    backgroundPosition: '100% 55%',
-    paddingLeft: '3rem',
-    paddingRight: '14rem',
-    display: 'flex'
-  },
-  filter: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    backdropFilter: 'blur(1.5rem)',
-    background: 'rgba(255, 255, 255, 0.8)'
-  },
   figure: {
-    position: 'relative',
     minWidth: '53.6rem',
     maxWidth: '53.6rem',
     height: '53.6rem'
