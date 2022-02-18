42d49629d8ce5e4d9cf6c6274974cf82c1309877
Mostafa Ghafouri <mstghafouri@gmail.com>
2022-02-18 20:15:09 +0330

    Create custom types file
    Add theme colors enum

---
+++ src/types/general-types.ts
@@ -0,0 +1,20 @@
+export enum ThemeVariant {
+  DARK = 'dark',
+  LIGHT = 'light'
+}
+
+export enum Color {
+  PRM_MAIN = 'var(--color-primary-main)',
+  PRM_LIGHT = 'var(--color-primary-light)',
+  PRM_LIGHTER = 'var(--color-primary-lighter)',
+  PRM_DARK = 'var(--color-primary-dark)',
+  PRM_DARKER = 'var(--color-primary-darker)',
+  SEC_MAIN = 'var(--color-secondary-main)',
+  SEC_DARK = 'var(--color-secondary-dark)',
+  LIGHT = 'var(--color-light)',
+  GREY = 'var(--color-grey)',
+  GREY_DARK = 'var(--color-grey-dark)',
+  GREY_LIGHT = 'var(--color-grey-light)',
+  WHITE = 'var(--color-white)',
+  BLACK = 'var(--color-black)'
+}
