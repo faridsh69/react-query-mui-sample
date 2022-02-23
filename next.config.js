fc5dc708ab84759fe62879932db25275a87f2d7f
Mostafa Ghafouri <mstghafouri@gmail.com>
2022-02-23 20:55:59 +0330

    Update next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    HOST: process.env.HOST
+  }
 };
 
 module.exports = nextConfig;
