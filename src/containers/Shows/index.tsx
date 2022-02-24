1ca6ecb97aa9bcd2629931243df681bb5a3b75b7
Mostafa Ghafouri <mstghafouri@gmail.com>
2022-02-24 19:59:45 +0330

    Fetch podcasts in shows page

--- src/containers/Shows/index.tsx
+++ src/containers/Shows/index.tsx
@@ -4,6 +4,7 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
+import { usePodcasts } from '@hooks/usePodcasts';
 import { ThemeVariant } from '@custom-types/general-types';
 import BlurBackground from '@components/BlurBG';
 import SearchBox from '@components/SearchBox';
@@ -11,38 +12,15 @@ import PodcastSlider from '@components/MultiSlider';
 import HeaderSlider from '@components/HeaderSlider';
 import { useStyles } from './styles';
 
-const DATA = [
-  {
-    image: '/images/poster.png',
-    title: 'The Happy Place',
-    category: 'Heath & Fitness',
-    description:
-      'Fearne Cotton talks to incredible people about life, love, loss, and everything in-between as she reveals what happiness means to them.'
-  },
-  {
-    image: '/images/absolute-mental.jpg',
-    title: 'Absolutely Mental',
-    category: 'Heath & Fitness',
-    description:
-      'Fearne Cotton talks to incredible people about life, love, loss, and everything in-between as she reveals what happiness means to them.'
-  },
-  {
-    image: '/images/under-the-skin.jpg',
-    title: 'Under The Skin',
-    category: 'Heath & Fitness',
-    description:
-      'Fearne Cotton talks to incredible people about life, love, loss, and everything in-between as she reveals what happiness means to them.'
-  }
-];
-
 const ShowsContainer: FC = () => {
   const { classes } = useStyles();
+  const [podcasts] = usePodcasts();
 
   return (
     <>
       <HeaderSlider>
-        {DATA.map(p => (
-          <BlurBackground key={p.title} size="md" transparent="true" img={p.image}>
+        {podcasts.map(p => (
+          <BlurBackground key={p.title} size="md" transparent="true" img={p.image.url}>
             <Stack direction="row" spacing={5} flex="1" pt="2rem" position="relative">
               <Stack direction="column" spacing={1} flex="1" className={classes.infoBox}>
                 <Typography variant="h1" component="p">
@@ -50,7 +28,7 @@ const ShowsContainer: FC = () => {
                 </Typography>
 
                 <Typography mb={1} variant="caption">
-                  {p.category}
+                  {p.itunes.categories?.[0]?.name || ''}
                 </Typography>
 
                 <Typography>{p.description}</Typography>
@@ -61,7 +39,7 @@ const ShowsContainer: FC = () => {
               </Stack>
 
               <figure className={classes.figure}>
-                <Image priority src={p.image} layout="fill" alt={p.title} />
+                <Image priority src={p.image.url} layout="fill" alt={p.title} />
               </figure>
             </Stack>
           </BlurBackground>
--- src/pages/shows/index.tsx
+++ src/pages/shows/index.tsx
@@ -1,12 +1,27 @@
 import type { NextPage } from 'next';
+import { dehydrate } from 'react-query';
 
 import ShowsContainer from '@containers/Shows';
 import HeadWrapper from '@components/HeadWrapper';
+import { getQueryClient, queryKeys } from '@utils/helpers';
+import { getPodcasts } from '@lib/podcast';
 
 const ShowsPage: NextPage = () => (
   <HeadWrapper title="Shows">
     <ShowsContainer />
   </HeadWrapper>
 );
+
+export async function getStaticProps() {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery(queryKeys.podcast.list(), () => getPodcasts());
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient)
+    }
+  };
+}
 
 export default ShowsPage;
