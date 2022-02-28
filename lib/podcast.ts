931d8146a7d3d8d3bc04440728cc41012a02e3f7
Mostafa Ghafouri <mstghafouri@gmail.com>
2022-02-28 10:49:59 +0330

    Create get-podcast-by-slug action

--- lib/podcast.ts
+++ lib/podcast.ts
@@ -4,10 +4,17 @@ import { Params } from '@interfaces/general';
 import { IPodcast } from '@interfaces/podcast';
 
 const INITIAL_PARAMS = {
-  select: 'title,image,description,itunes.categories,itunes.season,itunes.episode'
+  select: 'title,slug,image,description,itunes.categories,itunes.season,itunes.episode'
 };
 
 export const getPodcasts = async (params: Params = INITIAL_PARAMS): Promise<IPodcast[]> => {
   const { data } = await viralTribeAPI.get('/podcasts', { params });
   return data?.data?.podcasts;
+};
+
+export const getPodcastBySlug = async (slug: string): Promise<IPodcast | undefined> => {
+  try {
+    const { data } = await viralTribeAPI.get(`/podcasts/slug/${slug}`);
+    return data?.data?.podcast;
+  } catch (err) {}
 };
